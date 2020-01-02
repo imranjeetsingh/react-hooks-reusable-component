ef9956f77a2ed77054523f8100936bdc78f99226
imranjeetsingh <ranjee970@gmail.com>
2020-01-02 23:42:50 +0530

    hooks

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+
+import ResourceList from './resourceList';
+import User from './UserList';
+
+const App = () => {
+  const [resource, setResource] = useState("posts");
+  return (
+    <div>
+      <User />
+      <div>
+        <button onClick={() => setResource("posts")}>Posts</button>
+        <button onClick={() => setResource("todos")}>Todos</button>
+      </div>
+      <ResourceList resource={resource} />
+    </div>
+  );
+};
+
+export default App;
